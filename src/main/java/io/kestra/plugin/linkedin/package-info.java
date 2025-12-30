0469c1796db34bd59ce4400b1fa04c1536b61ddd
ben8t <pimpaudben@gmail.com>
2025-12-30 15:39:24 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/linkedin/package-info.java
+++ src/main/java/io/kestra/plugin/linkedin/package-info.java
@@ -1,7 +1,10 @@
 @PluginSubGroup(
     title = "Example plugin",
     description = "A plugin to show how to build a plugin in Kestra.",
-    categories = PluginSubGroup.PluginCategory.TOOL
+    categories = PluginSubGroup.PluginCategory.TOOL,
+    categories = {
+        PluginSubGroup.PluginCategory.BUSINESS
+    }
 )
 package io.kestra.plugin.linkedin;
 
