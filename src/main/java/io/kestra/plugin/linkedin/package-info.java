20f6c3035026fc497ddb3528678ced220d2b5025
François Delbrayelle <fdelbrayelle@gmail.com>
2026-02-13 08:09:31 +0100

    chore: update plugin categories (#22)
    * chore: update plugin categories
    
    * chore: update kestraVersion to 1.2.3
    
    * chore: merge package categories attribute
    
    ---------
    
    Co-authored-by: ben8t <pimpaudben@gmail.com>
    Co-authored-by: François Delbrayelle <fdelbrayelle@kestra.io>
    Co-authored-by: Malay Dewangan <66718045+Malaydewangan09@users.noreply.github.com>

--- src/main/java/io/kestra/plugin/linkedin/package-info.java
+++ src/main/java/io/kestra/plugin/linkedin/package-info.java
@@ -1,7 +1,8 @@
 @PluginSubGroup(
     title = "Example plugin",
-    description = "A plugin to show how to build a plugin in Kestra.",
-    categories = PluginSubGroup.PluginCategory.TOOL
+    description = "A plugin to show how to build a plugin in Kestra.",categories = {
+        PluginSubGroup.PluginCategory.BUSINESS
+    }
 )
 package io.kestra.plugin.linkedin;
 
